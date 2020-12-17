104ba07dab2af81d1340b5c78e4c1adf9035818c
Max Kohne <kohne.max@gmail.com>
2020-12-17 14:21:04 -0800

    fix DetailsViewBindingMemoryLeakFragmentBinding

--- app/src/main/java/com/kohne/memoryleaks/viewbindingleak/DetailsViewBindingMemoryLeakFragment.kt
+++ app/src/main/java/com/kohne/memoryleaks/viewbindingleak/DetailsViewBindingMemoryLeakFragment.kt
@@ -5,17 +5,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.kohne.memoryleaks.databinding.DetailsMemoryLeakViewFragmentBinding
+import com.kohne.memoryleaks.databinding.DetailsViewBindingMemoryLeakFragmentBinding
 
 internal class DetailsViewBindingMemoryLeakFragment : Fragment() {
 
-    private lateinit var binding: DetailsMemoryLeakViewFragmentBinding
+    private lateinit var binding: DetailsViewBindingMemoryLeakFragmentBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = DetailsMemoryLeakViewFragmentBinding.inflate(inflater, container, false)
+        binding = DetailsViewBindingMemoryLeakFragmentBinding.inflate(inflater, container, false)
         return binding.root
     }
 
