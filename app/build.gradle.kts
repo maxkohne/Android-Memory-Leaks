7ebd7761b63dce2972ae8351d9ca915eac84ba3f
Max Kohne <kohne.max@gmail.com>
2024-09-24 09:52:15 -0700

    compile and target sdk 34 -> 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.kohne.memoryleaks"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.kohne.memoryleaks"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
